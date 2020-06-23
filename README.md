3f839aed0f574e080b7e66cf61adeec5b8945617
Xiaoling Peng <pengmaradi@gmail.com>
2020-06-23 19:59:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# webpack
+
+## first run 
+```npm install```
+
+## then run npm dev
+```npm run dev```
+
---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * no touch on this file!
+ * Xiaoling Peng 2019-04-22
+ */
+
+import _ from 'lodash';
+
+// sass
+import './Resources/Public/Sass/index.scss';
+
+
+// javascript
+import './Resources/Public/JavaScript/index';
